cd5171e078a1bbd49c127f95b2c48c6ba634c902
Enrique Talavera <etalavera.ordaz@gmail.com>
2018-10-03 22:02:39 -0500

    add login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ControlPanelComponent } from './control-panel/control-panel.component';
+import { FormsModule } from '@angular/forms';
+import { LoginGuard } from './_guards/login.guard';
+import { NoLoginGuard } from './_guards/no-login.guard';
+
+const routes: Routes = [
+  { path: '', component: ControlPanelComponent, canActivate: [LoginGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [NoLoginGuard] }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ControlPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //NgbModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [LoginGuard, NoLoginGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
